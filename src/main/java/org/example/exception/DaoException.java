26138b460d148fcc86e09411669729be496e3059
Dioneer <gulkina_em@mail.ru>
2026-05-29 16:17:31 +0300

    ready for service create

--- src/main/java/org/example/exception/DaoException.java
+++ src/main/java/org/example/exception/DaoException.java
@@ -1,7 +1,5 @@
 package org.example.exception;
 
-import java.sql.SQLException;
-
 public class DaoException extends RuntimeException {
     public DaoException(String message, Throwable e) {
         super(message, e);
